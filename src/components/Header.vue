f9fdb0864898cf3b4fef7b5bc2dcbcf2ea55d231
Mohammed Khalil <muhamad.khalil12345@gmail.com>
2022-07-02 21:23:25 +0300

    First

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="">
+        <div class="logo flex justify-between">
+            <p>Mohammed Khalil</p>
+            <div class="hamburger">
+                <div class="hamburger-1" id="hamburger">
+                    <span class="line"></span>
+                    <span class="line"></span>
+                    <span class="line"></span>
+                </div>
+            </div>
+        </div>
+        <nav class="hidden">
+            <ul class="md:flex">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Skills</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+        </nav>
+    </header>
+</template>
---
+++ src/main.js
@@ -0,0 +1,23 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import '../index.css'
+import '../public/static/style/style.css'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret,faBars)
+
+const app = createApp(App).component('font-awesome-icon', FontAwesomeIcon)
+
+app.use(router)
+
+app.mount('#app')
