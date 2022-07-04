dedcfd2691e0979368fbf74b8c8fa7205962b62a
Mohammed Khalil <muhamad.khalil12345@gmail.com>
2022-07-04 23:03:02 +0300

    home responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -57,7 +57,7 @@
         </div>
     </header>
 
-    <nav class="text-white text-xl  lg:invisible bg-[#12141D] overflow-hidden transition-all duration-500"
+    <nav class="text-white text-xl fixed top-[15vh] w-full lg:invisible bg-[#12141D] overflow-hidden transition-all duration-500"
         :class="show?'visible h-[85vh]':'invisible h-0'"
                 >
             <p   class="
@@ -85,6 +85,15 @@
                         px-8
                         ">About</p>
         </nav>
+
+        <div class="aboutme md:flex md:h-[85vh] md:items-center md:justify-evenly py-4 md:p-0 w-full">
+            <div class="nusin py-8 md:p-0 md:w-full">
+                <p class="text-xl text-center md:text-3xl text-slate-400 font-light">Hello, my name is <span class="text-red-500">Mohammed Khalil</span> <br> and iam a <span class="text-slate-600">FullStack developer</span>  <br> Wllcome to my <span class="text-red-500">Portfolio</span></p>
+            </div>
+            <div class="imagej rounded-full py-8 md:p-0">
+                    <img src="../../public/static/images/me.jpg" alt="" srcset="" class="h-[70%] w-[70%] sm:h-[50%] sm:w-[50%] m-auto md:w-[40%] md:h-[70%] lg:w-[50%] lg:h-[70%] rounded-full">
+            </div>
+        </div>
 </template>
 
 <script>
